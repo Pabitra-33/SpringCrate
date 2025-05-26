ae0099be9db58e6e2847ee93680f4874a1188f80
Pabitra-33 <kumarpabitra345@gmail.com>
2025-05-26 20:39:34 +0530

    created a Person as a pojo class

--- springcore/src/main/java/xmlfile/classes/Person.java
+++ springcore/src/main/java/xmlfile/classes/Person.java
@@ -1,5 +1,9 @@
 package xmlfile.classes;
 
 public class Person {
-
+	String name = "Raju";//hard-coding, giving name manually
+	
+	public void walk() {
+		System.out.println(name+ "can Walk...!");
+	}
 }
