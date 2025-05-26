08bb6a16ebd7031edeac7ee2eb717e386fe68b49
Pabitra-33 <kumarpabitra345@gmail.com>
2025-05-26 20:43:11 +0530

    changing the implementation in the method of person class

--- springcore/src/main/java/xmlfile/classes/Person.java
+++ springcore/src/main/java/xmlfile/classes/Person.java
@@ -4,6 +4,6 @@ public class Person {
 	String name = "Raju";//hard-coding, giving name manually
 	
 	public void walk() {
-		System.out.println(name+ "can Walk...!");
+		System.out.println(name+ " can Walk...!");
 	}
 }
