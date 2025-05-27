d49dfc4ee76ea4317bd5c37f170d754de753429a
Pabitra-33 <kumarpabitra345@gmail.com>
2025-05-27 18:02:50 +0530

    performing the implementing subclass and created another ioc container to perform for another class

--- springcore/src/main/java/xmlfile/drivers/Driver.java
+++ springcore/src/main/java/xmlfile/drivers/Driver.java
@@ -3,11 +3,12 @@ package xmlfile.drivers;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import xmlfile.classes.Mobile;
 import xmlfile.classes.Person;
 
 public class Driver {
 	public static void main(String[] args) {
-		//IOC Container
+		//IOC Container-1
 		ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
 		
 		//down-cast to get Person type data
@@ -15,5 +16,15 @@ public class Driver {
 		
 		//call the person method
 		per.walk();
+		
+		
+		//IOC Container-1
+		ApplicationContext context1 = new ClassPathXmlApplicationContext("config.xml");
+				
+		//down-cast to get Person type data
+		Mobile mob = (Mobile)context1.getBean("mobile");
+				
+	    //call the person method
+		mob.call();
 	}
 }
