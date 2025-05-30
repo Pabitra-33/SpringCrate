9f6125c9aecb33b4259f7f95252932e92cddb06e
Pabitra-33 <kumarpabitra345@gmail.com>
2025-05-31 00:03:48 +0530

    added the @Qualifier annotation to make it a bean object

--- springcore/src/main/java/javabased/classes/Person.java
+++ springcore/src/main/java/javabased/classes/Person.java
@@ -1,9 +1,11 @@
 package javabased.classes;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
+@Qualifier("vivo")
 public class Person {
 	
 	@Autowired
