e3ba3163538d72665cdadeaa65e76dc279ebc934
Pabitra-33 <kumarpabitra345@gmail.com>
2025-05-31 00:03:18 +0530

    added the primary annotation to make it default bean for our class

--- springcore/src/main/java/javabased/classes/Redmi.java
+++ springcore/src/main/java/javabased/classes/Redmi.java
@@ -1,8 +1,10 @@
 package javabased.classes;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+@Primary
 public class Redmi implements Mobile {
 
 	@Override
