d9a20e9f438435665c86ec126ba8263d798c5070
Pabitra-33 <kumarpabitra345@gmail.com>
2025-05-31 10:41:51 +0530

    initial commit

---
+++ webspringcrudapp/src/main/java/com/webcrudapp/WebspringcrudappApplication.java
@@ -0,0 +1,13 @@
+package com.webcrudapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebspringcrudappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebspringcrudappApplication.class, args);
+	}
+
+}
---
+++ webspringcrudapp/src/test/java/com/webcrudapp/WebspringcrudappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webcrudapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebspringcrudappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
