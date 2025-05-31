b7d804aae80515dfc37cde89cf0002e750022d57
Pabitra-33 <kumarpabitra345@gmail.com>
2025-05-31 10:05:55 +0530

    made some new configurations to our class to access our built-in class Car

--- springcore/src/main/java/javabased/config/MyConfig.java
+++ springcore/src/main/java/javabased/config/MyConfig.java
@@ -1,10 +1,20 @@
 package javabased.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+import javabased.classes.Car;
 
 @Configuration
 @ComponentScan(basePackages = "javabased.classes")
 public class MyConfig {
-
+	
+	//creating a method to return the pre-defined class object
+	//as we don't have access to it, so we use @bean annotation.
+	@Bean
+	public Car getCar() {
+		return new Car();
+	}
 }
