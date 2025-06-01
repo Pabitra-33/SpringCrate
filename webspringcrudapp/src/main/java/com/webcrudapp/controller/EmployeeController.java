365a23abdd9d00d1c9ee393c2bef2e8ad2e2d58b
Pabitra-33 <kumarpabitra345@gmail.com>
2025-06-01 18:05:11 +0530

    created a save method with end-points to save the data

--- webspringcrudapp/src/main/java/com/webcrudapp/controller/EmployeeController.java
+++ webspringcrudapp/src/main/java/com/webcrudapp/controller/EmployeeController.java
@@ -1,11 +1,25 @@
 package com.webcrudapp.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.webcrudapp.entity.Employee;
+import com.webcrudapp.repository.EmployeeRepository;
 
 @RestController
 @RequestMapping("/api/employee") //base end-point or url
 public class EmployeeController { 
 	
+	@Autowired
+	private EmployeeRepository employeeRepository;
 	
+	@PostMapping("/save")
+	public String savedata(@RequestBody Employee employee) {
+		//calling the JPA interface save method to save data
+		employeeRepository.save(employee);
+		return "Data Saved...";
+	}
 }
