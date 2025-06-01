52ff477319266ece4d38878a192eafc867264d66
Pabitra-33 <kumarpabitra345@gmail.com>
2025-06-01 18:00:37 +0530

    created a controller class to interact and gives the business logic

---
+++ webspringcrudapp/src/main/java/com/webcrudapp/controller/EmployeeController.java
@@ -0,0 +1,11 @@
+package com.webcrudapp.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/employee") //base end-point or url
+public class EmployeeController { 
+	
+	
+}
