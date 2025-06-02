3bbdc3e47c716450d4b878f9b7b7763cd359c758
Pabitra-33 <kumarpabitra345@gmail.com>
2025-06-02 10:47:03 +0530

    created a update method with a rest end-point to update all the data

--- webspringcrudapp/src/main/java/com/webcrudapp/controller/EmployeeController.java
+++ webspringcrudapp/src/main/java/com/webcrudapp/controller/EmployeeController.java
@@ -1,10 +1,12 @@
 package com.webcrudapp.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,10 +16,11 @@ import com.webcrudapp.repository.EmployeeRepository;
 
 @RestController
 @RequestMapping("/api/employee") //base end-point or url
-public class EmployeeController { 
+public class EmployeeController {
 	
 	@Autowired
-	private EmployeeRepository employeeRepository; //automatic dependency injection
+	private EmployeeRepository employeeRepository;
+
 	
 	@PostMapping("/save")
 	public String savedata(@RequestBody Employee employee) {
@@ -30,5 +33,16 @@ public class EmployeeController {
 	@GetMapping("/getall")
 	public List<Employee> getAll(){
 		return employeeRepository.findAll();
+	}
+	
+	
+	@PutMapping("/update") //while updating full object we need to go for putmapping
+	public String update(@RequestBody Employee emp) {
+		int id = emp.getId();//get the id
+		Optional<Employee> optional = employeeRepository.findById(id);
+		Employee e1 = optional.get();
+		System.out.println(e1);
+		employeeRepository.save(emp);
+		return "data updated";
 	}
 }
