b729faeaa30fe64f94ff4b8959923d55629cddbb
Pabitra-33 <kumarpabitra345@gmail.com>
2025-06-03 09:49:25 +0530

    creating our own custom method inside the repository interface to do some modified taks

--- webspringcrudapp/src/main/java/com/webcrudapp/repository/EmployeeRepository.java
+++ webspringcrudapp/src/main/java/com/webcrudapp/repository/EmployeeRepository.java
@@ -1,6 +1,9 @@
 package com.webcrudapp.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.webcrudapp.entity.Employee;
@@ -8,4 +11,10 @@ import com.webcrudapp.entity.Employee;
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Integer>{
 
+	//creation of own custom method
+	
+	@Query("FROM Employee where code=:code")
+	public List<Employee> getAllByCode(int code);
+	
+	
 }
