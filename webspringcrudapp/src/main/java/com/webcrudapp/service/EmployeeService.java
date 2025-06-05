d7a8ab31f704f1927363780607a6a4488e56cf22
Pabitra-33 <kumarpabitra345@gmail.com>
2025-06-05 20:39:55 +0530

    created a service layer and provided the business logic codes to perform loose coupling and abstraction

--- webspringcrudapp/src/main/java/com/webcrudapp/service/EmployeeService.java
+++ webspringcrudapp/src/main/java/com/webcrudapp/service/EmployeeService.java
@@ -1,5 +1,18 @@
 package com.webcrudapp.service;
 
+import java.util.List;
+
+import com.webcrudapp.entity.Employee;
+
 public interface EmployeeService {
 
+	//creating abstract methods
+	
+	public Employee save(Employee employee);//to save data
+	
+	public Employee update(Employee employee);//to update data
+	
+	public List<Employee> getAll();
+	
+	public Employee deleteById(int id);
 }
--- webspringcrudapp/src/main/java/com/webcrudapp/service/EmployeeServiceImplementation.java
+++ webspringcrudapp/src/main/java/com/webcrudapp/service/EmployeeServiceImplementation.java
@@ -1,8 +1,40 @@
 package com.webcrudapp.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service
-public class EmployeeServiceImplementation implements EmployeeService{
+import com.webcrudapp.entity.Employee;
+import com.webcrudapp.repository.EmployeeRepository;
 
+@Service
+public class EmployeeServiceImplementation implements EmployeeService {
+
+	@Autowired
+	private EmployeeRepository employeeRepository;
+
+	@Override
+	public Employee save(Employee employee) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Employee update(Employee employee) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Employee> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Employee deleteById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
