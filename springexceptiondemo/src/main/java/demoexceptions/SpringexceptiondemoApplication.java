d3063afcaa2d150ce81613bc6397bb7d9a997296
Pabitra-33 <kumarpabitra345@gmail.com>
2025-07-31 23:43:27 +0530

    made some little changes in the main file

--- springexceptiondemo/src/main/java/demoexceptions/SpringexceptiondemoApplication.java
+++ springexceptiondemo/src/main/java/demoexceptions/SpringexceptiondemoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringexceptiondemoApplication {
 
+	//main starting part of our spring project
 	public static void main(String[] args) {
 		SpringApplication.run(SpringexceptiondemoApplication.class, args);
 	}
-
 }
