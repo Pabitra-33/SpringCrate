478c1251326899904b1b0c39eb6536d64a57ff36
Pabitra-33 <kumarpabitra345@gmail.com>
2025-07-31 23:49:57 +0530

    creating this to interact with the fronted.

--- springexceptiondemo/src/main/java/demoexceptions/controller/StudentController.java
+++ springexceptiondemo/src/main/java/demoexceptions/controller/StudentController.java
@@ -1,8 +1,11 @@
 package demoexceptions.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/students")
 public class StudentController {
 
+	
 }
