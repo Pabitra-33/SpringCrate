d2789b381a5c768c06bff16e5ddfcd9b2edf7a28
Pabitra-33 <kumarpabitra345@gmail.com>
2025-08-01 23:19:04 +0530

    provided the implementation for those methods in our class

--- springexceptiondemo/src/main/java/demoexceptions/service/StudentServiceImplementation.java
+++ springexceptiondemo/src/main/java/demoexceptions/service/StudentServiceImplementation.java
@@ -2,17 +2,21 @@ package demoexceptions.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import demoexceptions.entity.Student;
+import demoexceptions.repository.StudentRepository;
 
 @Component
 public  class StudentServiceImplementation implements  StudentService {
 
+	@Autowired
+	private StudentRepository studentRepository;
+	
 	@Override
 	public Student saveStudent(Student student) {
-		// TODO Auto-generated method stub
-		return null;
+		return studentRepository.save(student);
 	}
 
 	@Override
