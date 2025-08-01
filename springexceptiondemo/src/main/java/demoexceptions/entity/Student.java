40359e24ca7d41e9a1640212a15cbc3dfe53d354
Pabitra-33 <kumarpabitra345@gmail.com>
2025-08-01 23:16:02 +0530

    added the getter and setter methods in the entity class

--- springexceptiondemo/src/main/java/demoexceptions/entity/Student.java
+++ springexceptiondemo/src/main/java/demoexceptions/entity/Student.java
@@ -3,11 +3,9 @@ package demoexceptions.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.Data;
 
 @Entity
 @Table(name = "student_info")
-@Data
 public class Student {
 	
 	@Id
@@ -15,4 +13,23 @@ public class Student {
 	private String name;
 	private int age;
 	
+	//getters and setters
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
 }
