0210ff221ac9d94d9539461632e8be84437b4868
Pabitra-33 <kumarpabitra345@gmail.com>
2025-08-02 08:30:22 +0530

    provided some of the end points to interact with the fronted or postman to test them

--- springexceptiondemo/src/main/java/demoexceptions/controller/StudentController.java
+++ springexceptiondemo/src/main/java/demoexceptions/controller/StudentController.java
@@ -1,11 +1,23 @@
 package demoexceptions.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import demoexceptions.entity.Student;
+import demoexceptions.service.StudentService;
 
 @RestController
 @RequestMapping("/students")
 public class StudentController {
 
+	@Autowired
+	private StudentService studentService;
+	
+	@PostMapping("/save") //will trigger for the "/students/save" URL
+	public Student saveStudent(Student student) {
+		return null;
+	}
 	
 }
