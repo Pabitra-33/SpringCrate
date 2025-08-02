a2686e9de74bcddf1a595bda80cad5e6370ccf58
Pabitra-33 <kumarpabitra345@gmail.com>
2025-08-02 08:29:43 +0530

    introduced the exception handling class to handle all exceptions

---
+++ springexceptiondemo/src/main/java/demoexceptions/exceptionhandling/ApplicationExceptionHandler.java
@@ -0,0 +1,9 @@
+package demoexceptions.exceptionhandling;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+//as we are dealing with distributed application, so we are using RestControllerAdvice
+@RestControllerAdvice 
+public class ApplicationExceptionHandler {
+	
+}
