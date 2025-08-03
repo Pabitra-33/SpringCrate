67370108816587ee4dabae3a12afa8131b83bdc7
Pabitra-33 <kumarpabitra345@gmail.com>
2025-08-03 11:06:57 +0530

    handled the custom exception coming in our program by Global Exception Handler

--- springexceptiondemo/src/main/java/demoexceptions/exceptionhandling/ApplicationExceptionHandler.java
+++ springexceptiondemo/src/main/java/demoexceptions/exceptionhandling/ApplicationExceptionHandler.java
@@ -1,9 +1,25 @@
 package demoexceptions.exceptionhandling;
 
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-//as we are dealing with distributed application, so we are using RestControllerAdvice
+//as we are dealing with distributed application,
+//so we are using RestControllerAdvice or we should use ControllerAdvice
+
 @RestControllerAdvice 
 public class ApplicationExceptionHandler {
 	
+	@ExceptionHandler(value = StudentNotFoundException.class)
+	public ResponseEntity<ExceptionInfo> studentIfNotFound(StudentNotFoundException stud)
+	{
+		ExceptionInfo info = new ExceptionInfo();
+		info.setCode("EXC001");
+		info.setMsg(stud.getMessage());
+		info.setLocalDateTime(LocalDateTime.now());
+		return new ResponseEntity<ExceptionInfo>(info, HttpStatus.CREATED);
+	}
 }
