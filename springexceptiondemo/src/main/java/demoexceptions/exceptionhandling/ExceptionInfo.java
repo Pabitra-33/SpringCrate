7a7fdf02e25ffa5bcdd0d365b746495da17c409b
Pabitra-33 <kumarpabitra345@gmail.com>
2025-08-03 07:40:51 +0530

    created a exceptioninfo class to share the exception details in a standard format

---
+++ springexceptiondemo/src/main/java/demoexceptions/exceptionhandling/ExceptionInfo.java
@@ -0,0 +1,30 @@
+package demoexceptions.exceptionhandling;
+
+import java.time.LocalDateTime;
+
+public class ExceptionInfo {
+
+	private String code;//to show a standard code
+	private String msg;//a standard message
+	private LocalDateTime localDateTime;//show date and time
+	
+	//getters and setters
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getMsg() {
+		return msg;
+	}
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+	public LocalDateTime getLocalDateTime() {
+		return localDateTime;
+	}
+	public void setLocalDateTime(LocalDateTime localDateTime) {
+		this.localDateTime = localDateTime;
+	}
+}
