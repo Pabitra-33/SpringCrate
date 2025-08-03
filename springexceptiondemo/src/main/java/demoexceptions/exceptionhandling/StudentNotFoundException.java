3badb6b384cb4988d22e93e28886dc0114484f9f
Pabitra-33 <kumarpabitra345@gmail.com>
2025-08-04 00:36:42 +0530

    made some changes to avoid the supress warning

--- springexceptiondemo/src/main/java/demoexceptions/exceptionhandling/StudentNotFoundException.java
+++ springexceptiondemo/src/main/java/demoexceptions/exceptionhandling/StudentNotFoundException.java
@@ -1,5 +1,6 @@
 package demoexceptions.exceptionhandling;
 
+@SuppressWarnings("serial")
 public class StudentNotFoundException extends RuntimeException{
 
 	public StudentNotFoundException(String msg) {
